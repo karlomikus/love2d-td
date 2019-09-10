112503e64f1dc813a7762f1506802bb515f4535b
Karlo Mikus <karlo.mikus1@gmail.com>
2019-09-10 14:15:38 +0200

    Add input, timer

--- main.lua
+++ main.lua
@@ -1,48 +1,32 @@
 Object = require "libs/classic"
+Input = require "libs/Input"
+Timer = require "libs/Timer"
 Circle = require("obj/Circle")
 HyperCircle = require("obj/HyperCircle")
 
 function love.load()
-    -- local object_files = {}
-    -- recursiveEnumerate('obj', object_files)
-    -- print(object_files)
-    -- requireFiles(object_files)
+    timer = Timer()
+    input = Input()
+    input:bind('mouse1', 'm1')
 
-    -- local exerciseCircle = Test()
-    -- exerciseCircle = Circle(400, 300, 50)
     exHyperCircle = HyperCircle(400, 300, 50, 10, 120)
+    circle = {radius = 24}
+    timer:tween(6, circle, {radius = 96}, 'in-out-cubic')
 end
 
 function love.update(dt)
+    timer:update(dt)
+    if input:pressed('m1') then print('pressed') end
 end
 
 
 function love.draw()
-    -- exerciseCircle:draw()
-    exHyperCircle:draw()
+    -- exHyperCircle:draw()
+    love.graphics.circle('fill', 400, 300, circle.radius)
 end
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
-    end
-end
-
-function recursiveEnumerate(folder, file_list)
-    local items = love.filesystem.getDirectoryItems(folder)
-    for _, item in ipairs(items) do
-        local file = folder .. "/" .. item
-        if love.filesystem.isFile(file) then
-            table.insert(file_list, file)
-        elseif love.filesystem.isDirectory(file) then
-            recursiveEnumerate(file, file_list)
-        end
-    end
-end
-
-function requireFiles(files)
-    for _, file in ipairs(files) do
-        local file = file.sub(1, -5)
-        require(file)
     end
 end
