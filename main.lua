534dbea588e285c2f704c342fa77bae458344f8e
Karlo Mikus <karlo.mikus1@gmail.com>
2019-09-10 13:06:08 +0200

    Restart, follow tut

--- main.lua
+++ main.lua
@@ -1,4 +1,13 @@
 function love.load()
+
+    local vertices = {
+        {20, 20},
+        {300, 40},
+        {300, 100},
+        {20, 100}
+    }
+    mesh = love.graphics.newMesh(vertices, "fan")
+
     love.physics.setMeter(32)
     world = love.physics.newWorld(0, 9.81*32, true)
 
@@ -24,9 +33,17 @@ function love.update(dt)
 end
 
 function love.draw()
+    -- love.graphics.draw(mesh)
+
     love.graphics.setColor(0.28, 0.63, 0.05)
     love.graphics.polygon("fill", objects.ground.body:getWorldPoints(objects.ground.shape:getPoints())) -- draw a "filled in" polygon using the ground's coordinates
 
     love.graphics.setColor(0.76, 0.18, 0.05)
     love.graphics.polygon("fill", objects.tank.body:getWorldPoints(objects.tank.shape:getPoints()))
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
 end
