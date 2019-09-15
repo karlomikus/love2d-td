a1fb25319f116b4b74fe9837bfa3a8cbd8c9e7e8
Karlo Mikus <contact@karlomikus.com>
2019-09-15 18:36:07 +0200

    Add scorched earth effect

--- obj/Explosion.lua
+++ obj/Explosion.lua
@@ -4,6 +4,7 @@ function Explosion:new(area, x, y, opts)
     Explosion.super.new(self, area, x, y, opts)
 
     self.radius = opts.radius or 20
+    self.scorched_earth = 4
 end
 
 function Explosion:update(dt)
@@ -11,9 +12,23 @@ function Explosion:update(dt)
 
     for y = -self.radius, self.radius, 1 do
         for x = -self.radius, self.radius, 1 do
-            if x*x+y*y <= self.radius * self.radius + self.radius then
+            if x*x+y*y <= self.radius * self.radius then
                 if self.x + x < self.area.map_image_data:getWidth() and self.y + y < self.area.map_image_data:getHeight() and self.x + x > 0 and self.y + y > 0 then
                     self.area.map_image_data:setPixel(self.x + x, self.y + y, 0, 1, 0, 0)
+                end
+            end
+        end
+    end
+
+    self.radius = self.radius + self.scorched_earth
+    for y = -self.radius, self.radius, 1 do
+        for x = -self.radius, self.radius, 1 do
+            if x*x+y*y <= self.radius * self.radius then
+                if self.x + x < self.area.map_image_data:getWidth() and self.y + y < self.area.map_image_data:getHeight() and self.x + x > 0 and self.y + y > 0 then
+                    local r, g, b, a = self.area.map_image_data:getPixel(self.x + x, self.y + y)
+                    if a > 0 then
+                        self.area.map_image_data:setPixel(self.x + x, self.y + y, 1, 0, 0, 1)
+                    end
                 end
             end
         end
