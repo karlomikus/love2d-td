061877e250a88c42d3745ce366931bfad6661445
Karlo Mikus <contact@karlomikus.com>
2019-09-20 23:25:14 +0200

    remove debug

--- main.lua
+++ main.lua
@@ -33,15 +33,6 @@ function love.load()
     bg_music:setLooping(true)
     bg_music:play()
 
-    debug = false
-
-    coll_debug = {}
-    coll_debug.x = 0
-    coll_debug.y = 0
-    coll_debug.w = 20
-    coll_debug.h = 20
-    coll_debug.hit = false
-
     input:bind('mouse1', 'm1')
     input:bind('w', 'up')
     input:bind('s', 'down')
@@ -58,39 +49,15 @@ end
 function love.update(dt)
     timer:update(dt)
     camera:update(dt)
-
-    coll_debug.x = love.mouse.getX() - (coll_debug.w/2)
-    coll_debug.y = love.mouse.getY() - (coll_debug.h/2)
-
-    if stage then stage:update(dt) end
-
-    if debug then
-        if stage.area:collided(coll_debug.x, coll_debug.y, coll_debug.w, coll_debug.h) then
-            coll_debug.hit = true
-        else
-            coll_debug.hit = false
-        end
-    end
+    stage:update(dt)
 end
 
 function love.draw()
     love.graphics.setBackgroundColor(19/255, 0, 56/255)
 
     effect(function()
-        if stage then stage:draw() end
+        stage:draw()
     end)
-
-    if debug then
-        if coll_debug.hit then
-            love.graphics.print("Collision hit", 100, 20)
-        else
-            love.graphics.print("Collision miss", 100, 20)
-        end
-    end
-
-    if debug then
-        love.graphics.rectangle("fill", coll_debug.x, coll_debug.y, coll_debug.w, coll_debug.h)
-    end
 end
 
 function love.keypressed(key)
