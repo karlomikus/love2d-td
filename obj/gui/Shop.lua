da9603e1671fe0ad5b3bf9c816220c3c18b8993d
Karlo Mikus <contact@karlomikus.com>
2019-10-03 19:03:48 +0200

    Fix shop pos

--- obj/gui/Shop.lua
+++ obj/gui/Shop.lua
@@ -12,10 +12,10 @@ function Shop:new(player)
     for i, item in pairs(weapons_pool) do
         i = i - 1
         local buttonRow = {
-            ButtonShop(string.format("%s", item.stock), 20, 60 + i * 40, 50, 30, false),    -- Quantity
-            ButtonShop(item.name, 70, 60 + i * 40, 360, 30, false),                                 -- Name
-            ButtonShop(string.format("$%s", item.price), 430, 60 + i * 40, 140, 30, false),         -- Price
-            Button("Buy", 600, 60 + i * 40, 60, 30, true, true, true)                               -- Buy button
+            ButtonShop(string.format("%s", item.stock), self.x + 20, 80 + i * 40, 50, 30, false),    -- Quantity
+            ButtonShop(item.name, self.x + 70, 80 + i * 40, 360, 30, false),                                 -- Name
+            ButtonShop(string.format("$%s", item.price), self.x + 430, 80 + i * 40, 140, 30, false),         -- Price
+            Button("Buy", self.x + 600, 80 + i * 40, 60, 30, true, true, true)                               -- Buy button
         }
         buttonRow[#buttonRow]:setAction(function ()
             self:buy(item)
@@ -55,7 +55,6 @@ function Shop:draw()
     love.graphics.print("Weapon shop", self.x + 20, 26)
 
     love.graphics.push()
-    -- love.graphics.translate(self.x, self.y)
     for i,r in ipairs(self.items) do
         for _,b in pairs(r) do
             b:draw()
