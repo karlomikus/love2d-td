565e298135253fabe4356f4945fb825757512c6c
Karlo Mikus <contact@karlomikus.com>
2019-10-06 18:08:39 +0200

    add weapon manager

--- obj/WeaponManager.lua
+++ obj/WeaponManager.lua
@@ -2,10 +2,14 @@ WeaponManager = Object:extend()
 
 function WeaponManager:new()
     self.current_wpi = nil
+    self.grace = 2
 end
 
 function WeaponManager:update(dt)
-    -- self.current_wpi = director.current_player:getCurrentItem().weapon_pool_item
+    self.current_wpi = director.current_player:getCurrentItem().weapon_pool_item
+    if director.current_player.state == "waiting" then
+        -- self.current_wpi
+    end
 end
 
 function WeaponManager:spawn()
--- obj/weapons/RocketTrail.lua
+++ obj/weapons/RocketTrail.lua
@@ -12,11 +12,6 @@ function RocketTrail:new(x, y, opts)
     self.timer:tween(0.1, self, {r = 0, c = {r = 245/255, g = 57/255, b = 0/255}}, 'linear', function() self.dead = true end)
 end
 
-function RocketTrail:update(dt)
-    RocketTrail.super.update(self, dt)
-
-end
-
 function RocketTrail:draw()
     love.graphics.setColor(self.c.r, self.c.g, self.c.b)
     love.graphics.circle('fill', self.x, self.y, self.r + randomp(-0.5, 0.5))
